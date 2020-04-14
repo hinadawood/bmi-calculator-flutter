6c79964c45e9f82e83a1aa948eebaef1f30a987b
Hina Dawood <hinadawood@MACBOOKPRO-95E1.gateway>
2020-04-14 16:29:07 +1000

    bmi calculator

---
+++ lib/components/reusable_card.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour, this.cardChild, this.onPressed});
+
+  final Color colour;
+  final Widget cardChild;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        height: 100,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: colour,
+        ),
+      ),
+    );
+  }
+}
